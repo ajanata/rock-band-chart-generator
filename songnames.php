395166a2b70ea7572a22eaefcae29495352ca72a
ajanata <ajanata@03eddfae-9d10-0410-9ea1-923ea9aa3041>
2008-03-18 02:31:45 +0000

    added all dlc song names to array
    git-svn-id: svn+ssh://ajanata.com/var/lib/svn-old/phpspopt/branches/phpspopt2@79 03eddfae-9d10-0410-9ea1-923ea9aa3041

---
+++ songnames.php
@@ -0,0 +1,167 @@
+<?php
+    // GH1
+    
+    
+    
+    
+    
+    // GH2
+    $NAMES["strutter"] = "Strutter";
+    $NAMES["monkeywrench"] = "Monkey Wrench";
+    $NAMES["killinginthenameof"] = "Killing in the Name";
+    $NAMES["xstream"] = "X-Stream";
+
+    
+    
+    // GH80s
+    
+    
+    
+    
+    // GH3
+    
+    
+    
+    
+    // Rock Band
+    
+    $NAMES["29fingers"] = "29 Fingers";
+    $NAMES["areyougonnabemygirl"] = "Are You Gonna Be My Girl";
+    $NAMES["ballroomblitz"] = "Ballroom Blitz";
+    $NAMES["blackholesun"] = "Black Hole Sun";
+    $NAMES["blitzkriegbop"] = "Blitzkrieg Bop";
+    $NAMES["blooddoll"] = "Blood Doll";
+    $NAMES["brainpower"] = "Brainpower";
+    $NAMES["cantletgo"] = "Can't Let Go";
+    $NAMES["celebrityskin"] = "Celebrity Skin";
+    $NAMES["cherubrock"] = "Cherub Rock";
+    $NAMES["creep"] = "Creep";
+    $NAMES["danicalifornia"] = "Dani California";
+    $NAMES["daylatedollarshort"] = "Day Late, Dollar Short";
+    $NAMES["deadonarrival"] = "Dead On Arrival";
+    $NAMES["detroitrockcity"] = "Detroit Rock City";
+    $NAMES["dontfearthereaper"] = "(Don't Fear) The Reaper";
+    $NAMES["electricversion"] = "Electric Version";
+    $NAMES["entersandman"] = "Enter Sandman";
+    $NAMES["epic"] = "Epic";
+    $NAMES["flirtinwithdisaster"] = "Flirtin' With Disaster";
+    $NAMES["foreplaylongtime"] = "Foreplay/Long Time";
+    $NAMES["foreplaylongtime_short"] = "Foreplay/Long Time (Short)";
+    $NAMES["gimmieshelter"] = "Gimmie Shelter";
+    $NAMES["gowiththeflow"] = "Go With the Flow";
+    $NAMES["greengrass"] = "Green Grass and High Tides";
+    $NAMES["greengrass_short"] = "Green Grass and High Tides (short)";
+    $NAMES["hereitgoesagain"] = "Here It Goes Again";
+    $NAMES["highwaystar"] = "Highway Star";
+    $NAMES["igetby"] = "I Get By";
+    $NAMES["imsosick"] = "I'm So Sick";
+    $NAMES["inbloom"] = "In Bloom";
+    $NAMES["ithinkimparanoid"] = "I Think I'm Paranoid";
+    $NAMES["learntofly"] = "Learn to Fly";
+    $NAMES["mainoffender"] = "Main Offender";
+    $NAMES["maps"] = "Maps";
+    $NAMES["missqueen"] = "Mississippi Queen";
+    $NAMES["nexttoyou"] = "Next To You";
+    $NAMES["nightmare"] = "Nightmare";
+    $NAMES["orangecrush"] = "Orange Crush";
+    $NAMES["outside"] = "Outside";
+    $NAMES["paranoid"] = "Paranoid";
+    $NAMES["pleasure"] = "Pleasure (Pleasure)";
+    $NAMES["reptilia"] = "Reptilia";
+    $NAMES["runtothehills"] = "Run to the Hills";
+    $NAMES["sabotage"] = "Sabotage";
+    $NAMES["sayitaintso"] = "Say it Ain't So";
+    $NAMES["seven"] = "Seven";
+    $NAMES["shouldistay"] = "Should I Stay or Should I Go";
+    $NAMES["suffragettecity"] = "Suffragette City";
+    $NAMES["thehandthatfeeds"] = "The Hand That Feeds";
+    $NAMES["timewehad"] = "Time We Had";
+    $NAMES["timmy"] = "Timmy and the Lords of the Underworld";
+    $NAMES["tomsawyer"] = "Tom Sawyer";
+    $NAMES["trainkeptarollin"] = "Train Kept a Rollin'";
+    $NAMES["trainkeptarollin_short"] = "Train Kept a Rollin' (Short)";
+    $NAMES["vasoline"] = "Vasoline";
+    $NAMES["wanteddeadoralive"] = "Wanted Dead or Alive";
+    $NAMES["waveofmutilation"] = "Wave of Mutilation";
+    $NAMES["welcomehome"] = "Welcome Home";
+    $NAMES["whenyouwereyoung"] = "When You Were Young";
+    $NAMES["wontgetfooled"] = "Won't Get Fooled Again";
+    $NAMES["wontgetfooled_short"] = "Won't Get Fooled Again (Short)";
+    
+    
+    // Rock Band DLC
+
+    $NAMES["3sand7s"] = "";
+    $NAMES["action"] = "";
+    $NAMES["allthesmallthings"] = "All the Small Things";
+    $NAMES["andjusticeforall"] = "...And Justice for All";
+    $NAMES["attack"] = "";
+    $NAMES["bangagong"] = "Bang a Gong";
+    $NAMES["beethovensc"] = "Beethoven's C***";
+    $NAMES["blackened"] = "Blackened";
+    $NAMES["brassinpocket"] = "";
+    $NAMES["buddyholly"] = "";
+    $NAMES["callingdrlove"] = "";
+    $NAMES["cantstandlosingyou"] = "Can't Stand Losing You";
+    $NAMES["caseyjones"] = "";
+    $NAMES["cherrybomb"] = "";
+    $NAMES["collector"] = "The Collector";
+    $NAMES["completecontrol"] = "";
+    $NAMES["crushcrushcrush"] = "";
+    $NAMES["dieallright"] = "";
+    $NAMES["dirtylittlesecret"] = "";
+    $NAMES["chinacatsunflower"] = "";
+    $NAMES["dontlookbackinanger"] = "Don't Look Back in Anger";
+    $NAMES["elscorcho"] = "";
+    $NAMES["everfalleninlove"] = "Ever Fallen In Love";
+    $NAMES["fortunateson"] = "Fortunate Son";
+    $NAMES["franklinstower"] = "";
+    $NAMES["hardtohandle"] = "Hard to Handle";
+    $NAMES["heroes"] = "";
+    $NAMES["ifoughtthelaw"] = "I Fought the Law";
+    $NAMES["ineedamiracle"] = "";
+    $NAMES["interstatelovesong"] = "";
+    $NAMES["jokerandthe"] = "";
+    $NAMES["jukeboxhero"] = "";
+    $NAMES["lasttraintoclarksville"] = "Last Train to Clarksville";
+    $NAMES["limelight"] = "";
+    $NAMES["littlesister"] = "";
+    $NAMES["liveforever"] = "Live Forever";
+    $NAMES["marchofthepigs"] = "March of the Pigs";
+    $NAMES["moonagedaydream"] = "";
+    $NAMES["movealong"] = "Move Along";
+    $NAMES["myironlung"] = "";
+    $NAMES["mysharona"] = "";
+    $NAMES["nib"] = "";
+    $NAMES["numberofthebeast"] = "";
+    $NAMES["perfectdrug"] = "The Perfect Drug";
+    $NAMES["queenbitch"] = "";
+    $NAMES["ridethelightning"] = "Ride the Lightning";
+    $NAMES["roam"] = "";
+    $NAMES["rockawaybeach"] = "Rockaway Beach";
+    $NAMES["rockrebellion"] = "Rock Rebellion";
+    $NAMES["roxanne"] = "Roxanne";
+    $NAMES["sextypething"] = "";
+    $NAMES["shake"] = "Shake";
+    $NAMES["shockwave"] = "Shockwave";
+    $NAMES["sicksicksick"] = "";
+    $NAMES["siva"] = "";
+    $NAMES["songwithamission"] = "";
+    $NAMES["sprode"] = "Sprode";
+    $NAMES["sugarmagnolia"] = "";
+    $NAMES["sweetleaf"] = "";
+    $NAMES["synchronicity2"] = "Synchronicity II";
+    $NAMES["teenagelobotomy"] = "";
+    $NAMES["tenspeed"] = "";
+    $NAMES["thekill"] = "The Kill";
+    $NAMES["truckin"] = "";
+    $NAMES["truthhitseverybody"] = "";
+    $NAMES["warpigs"] = "War Pigs";
+    $NAMES["wecarealot"] = "";
+    $NAMES["whydoyouloveme"] = "";
+    $NAMES["wonderwall"] = "Wonderwall";
+    $NAMES["workingman"] = "";
+
+    
+
+?>
